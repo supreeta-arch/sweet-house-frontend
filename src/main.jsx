8321f0df346879419c7fc089180f33d397e7ff6d
supreeta-arch <preetu19052017@gmail.com>
2026-01-26 20:45:43 -0500

    main.js fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import { HashRouter } from "react-router-dom";
+import App from "./App";
 import { CartProvider } from "./context/CartContext";
 import { AdminProvider } from "./context/AdminContext";
 import "./index.css";
